0fce436268fd75d5c673a3f3afdf6946f778582a
love-pixel <197wangming@gmail.com>
2019-09-20 20:41:05 +0800

    the implementation of device in linux use xcb

--- src/device/device.c
+++ src/device/device.c
@@ -1,9 +1,12 @@
 #include "../../include/lpw/device/device_prototype.h"
 #include "../../include/lpw/device/platform_data_prototype.h"
+#include "../../include/lpw/device/event_handler.h"
 
 
 #include <malloc.h>
 #include <assert.h>
+
+void _lpwInitPlatformData(LpwPlatformData data);
 
 LpwDevice lpwCreateDevice(const LpwDeviceCreateInfo* ci_ptr)
 {
@@ -11,13 +14,9 @@ LpwDevice lpwCreateDevice(const LpwDeviceCreateInfo* ci_ptr)
     device->root = LPW_NULL_HANDLE;
     device->user_ptr = NULL;
     device->platform_data = (LpwPlatformData_Prototype*)malloc(sizeof(LpwPlatformData_Prototype));
-    {
-        device->platform_data->wnd_class_name = NULL;
-        device->platform_data->instance = NULL;
-        device->platform_data->hwnd = NULL;
-    }
+    _lpwInitPlatformData(device->platform_data);
     device->window = LPW_NULL_HANDLE;
-    //device->mouse = LPW_NULL_HANDLE;
+    device->mouse = LPW_NULL_HANDLE;
     //device->keyboard = LPW_NULL_HANDLE;
     if(ci_ptr != NULL)
     {
@@ -31,6 +30,15 @@ void lpwDestroyDevice(LpwDevice device)
     assert(device != LPW_NULL_HANDLE);
     free(device->platform_data);
     free(device);
+}
+
+#if defined( LPW_MACRO_USE_PLATFORM_SDK_WINDOWS )
+
+void _lpwInitPlatformData(LpwPlatformData data)
+{
+    data->wnd_class_name = NULL;
+    data->instance = NULL;
+    data->hwnd = NULL;
 }
 
 void lpwProcessEvent(LpwDevice device, LpwEnumProcessEventType type)
@@ -58,6 +66,56 @@ void lpwProcessEventByWait(LpwDevice device)
     WaitMessage();
     lpwProcessEventByPoll(device);
 }
+#elif defined( LPW_MACRO_USE_PLATFORM_SDK_XCB )
+
+void _lpwInitPlatformData(LpwPlatformData data)
+{
+    data->connection = NULL;
+    data->screen = NULL;
+    data->window = 0;
+    data->atom_window_reply = NULL;
+}
+
+void lpwProcessEvent(LpwDevice device, LpwEnumProcessEventType type)
+{
+    assert(device != LPW_NULL_HANDLE);
+    assert(device->platform_data->connection != NULL);
+
+    xcb_generic_event_t* xcb_event = NULL;
+    if(type == LPW_ENUM_PROCESS_EVENT_TYPE_POLL)
+        xcb_event = xcb_poll_for_event(device->platform_data->connection);
+    else
+    if(type == LPW_ENUM_PROCESS_EVENT_TYPE_WAIT)
+        xcb_event = xcb_wait_for_event(device->platform_data->connection);
+    if(xcb_event == NULL) return;
+
+    _lpwDeviceEventHandler(xcb_event, device);
+    free(xcb_event);
+}
+void lpwProcessEventByPoll(LpwDevice device)
+{
+    assert(device != LPW_NULL_HANDLE);
+    assert(device->platform_data->connection != NULL);
+
+    xcb_generic_event_t* xcb_event = xcb_poll_for_event(device->platform_data->connection);
+    if(xcb_event == NULL) return;
+
+    _lpwDeviceEventHandler(xcb_event, device);
+    free(xcb_event);
+}
+void lpwProcessEventByWait(LpwDevice device)
+{
+    assert(device != LPW_NULL_HANDLE);
+    assert(device->platform_data->connection != NULL);
+
+    xcb_generic_event_t* xcb_event = xcb_wait_for_event(device->platform_data->connection);
+    if(xcb_event == NULL) return;
+
+    _lpwDeviceEventHandler(xcb_event, device);
+    free(xcb_event);
+}
+
+#endif
 
 void lpwSetDeviceUserPtr(LpwDevice device, const void* src_ptr)
 {
