5588228c55fd2da2afff8d1a7fc6d84b3829cae9
love-pixel <197wangming@gmail.com>
2019-09-20 20:44:43 +0800

    add some members to it, to support the implementation in linux use xcb(double click)

--- include/lpw/device/mouse/mouse_prototype.h
+++ include/lpw/device/mouse/mouse_prototype.h
@@ -2,12 +2,19 @@
 
 #include "mouse.h"
 
+#include "../../macro/platform_sdk.h"
+#include "../../external/platform/platform_sdk.h"
+
 typedef struct LpwMouseInfo
 {
     LpwVec2_ui32            pos;
     LpwVec2_ui32            pre_pos;
     LpwEnumMouseButtonState button_state_table[LPW_ENUM_MOUSE_BUTTON_MAX_ENUM];
     LpwMousePfnCallbackList pfn_callback_list;
+#if defined( LPW_MACRO_USE_PLATFORM_SDK_XCB )
+    xcb_timestamp_t         pre_time_table[LPW_ENUM_MOUSE_BUTTON_MAX_ENUM];
+    uint32_t                time_interval;
+#endif
 }LpwMouseInfo;
 
 typedef struct LPW_MAKE_HANDLE_PROTOTYPE_NAME(LpwMouse)
